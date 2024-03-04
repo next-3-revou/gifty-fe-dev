dcdd8f52dddc536b676c32da71df8a11b433109f
Ade Kresna D <45942312+vaiosyndicates@users.noreply.github.com>
2024-03-04 20:05:18 +0700

    refactor: refactor profile page and add button as design

--- src/component/atom/Button/index.jsx
+++ src/component/atom/Button/index.jsx
@@ -1,15 +1,43 @@
 /* eslint-disable react/prop-types */
 import './styles.css'
+import { Button } from 'antd';
+import { UserOutlined, PlusOutlined, GiftOutlined } from '@ant-design/icons';
+import './styles.css'
 
-const Buttons = ({title, size, onClick}) => {
-  return (
-    // <button className="bg-[#1FAD66] text-white font-bold w-24 h-6 rounded-full">
-    // {title}
-    // </button>
-    <button className={`bg-[#1FAD66] text-white w-24 h-6 rounded-full p-0 `} onClick={onClick}>
-      <p className={`text-${size ? 'lg' : 'sm'}`}>{title}</p>
-    </button>    
-  )
+const Buttons = ({title, size, onClick,isIcon, types}) => {
+  if(isIcon) {
+    if(types === "profile") {
+      return (
+        <>
+          <Button type="primary" icon={<UserOutlined />} shape="circle" className='bg-[#1FAD66]' size={"large"}  onClick={onClick} />
+          <h4 className='text-black'>User</h4>         
+        </>
+
+      )
+    } else if(types === "add") {
+      return (
+        <>
+          <Button type="primary" icon={<PlusOutlined />} shape="circle" className='bg-[#1FAD66] h-16 !w-16' size={"large"} onClick={onClick} /> 
+          <h4 className='text-black'>Add List</h4>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <Button type="primary" icon={<GiftOutlined />} shape="circle" className='bg-[#1FAD66]' size={"large"}  onClick={onClick} /> 
+          <h4 className='text-black'>History</h4>
+        </>
+      )
+    }
+
+  } else {
+    return (
+      <button className={`bg-[#1FAD66] text-white w-24 h-6 rounded-full p-0 `} onClick={onClick}>
+        <p className={`text-${size ? 'lg' : 'sm'}`}>{title}</p>
+      </button>    
+    )
+  }
+
 }
 
 export default Buttons
--- src/component/molecules/Navbar/index.jsx
+++ src/component/molecules/Navbar/index.jsx
@@ -3,17 +3,33 @@
 import Buttons from "../../atom/Button"
 
 const Navbar = () => {
+
+	const profilePage = (e) => { 
+		e.preventDefault();
+		console.log('tes')
+	}
+
+	const addWishlist = (e) => {
+		e.preventDefault();
+		console.log('add')
+	}
+
+	const historyPage = (e) => {
+		e.preventDefault();
+		console.log('history')
+	}
+
 	return (
 		<div className="fixed inset-x-0 bottom-0 z-10 mx-auto flex min-h-[72px] max-w-[425px] items-center bg-white p-4">
-			<div className="flex w-full justify-center ">
+			<div className="flex w-full justify-evenly items-end ">
 				<div className="menus px-2">
-					<Buttons title={"User"} size={"lg"} />
+					<Buttons isIcon={true} types={"profile"} title={"User"} size={"lg"} onClick={(e) => profilePage(e)} />
 				</div>
 				<div className="menus px-2">
-					<Buttons title={"List"} size={"lg"} />
+					<Buttons isIcon={true} types={"add"} title={"User"} size={"lg"} onClick={(e) => addWishlist(e)} />
 				</div>
 				<div className="menus px-2">
-					<Buttons title={"History"} size={"lg"} />
+					<Buttons isIcon={true} types={"history"} title={"User"} size={"lg"} onClick={(e) => historyPage(e)} />
 				</div>
 			</div>
 		</div>
