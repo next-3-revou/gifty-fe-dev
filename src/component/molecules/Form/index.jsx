49e3ed81896ffc4dab4dfc9f811bf2809abd89e2
Ade Kresna Dewantara <821197@hq.bni.co.id>
2024-03-08 11:27:39 +0700

    feat: add axios, formik, yup

--- src/component/molecules/Form/index.jsx
+++ src/component/molecules/Form/index.jsx
@@ -1,15 +1,11 @@
 /* eslint-disable react/prop-types */
 // import React from 'react'
-
-import Buttons from "../../atom/Button"
-import { Select } from 'antd';
 // import { Routes, Route, useParams, useNavigate  } from 'react-router-dom';
 import './styles.css'
 import FormSignUp from "../FormSignUp";
 import FormAvatar from "../FormAvatar";
 import FormPayment from "../FormPayment";
 import FormPersonalWL from "../FormPersoWL";
-const { Option } = Select;
 
 const Forms = ({type, onClick, title}) => {
 
